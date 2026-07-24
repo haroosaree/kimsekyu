4d814cb3c087843e0849199e1c2a4d5fe6cef988
Alex J <alexbyj@gmail.com>
2026-07-24 17:28:43 -0500

    build fix

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 import { withPayload } from "@payloadcms/next/withPayload";
 
-const nextConfig: NextConfig = {};
+// const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  serverExternalPackages: ['drizzle-kit'],
+  // ...your existing config (withPayload wrapper, etc.)
+}
 
 export default withPayload(nextConfig);
