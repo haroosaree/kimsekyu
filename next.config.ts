6929d946d9280a99a5fc0981fce5eb5f15a5beff
Alex J <alexbyj@gmail.com>
2026-07-28 00:04:37 -0500

    build for cf

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 import { withPayload } from "@payloadcms/next/withPayload";
 
 const nextConfig: NextConfig = {
-    serverExternalPackages: ['drizzle-kit', '@payloadcms/drizzle'],
+    serverExternalPackages: ['drizzle-kit', '@payloadcms/drizzle', 'jose', 'pg-cloudflare'],
 };
 
 export default withPayload(nextConfig);
