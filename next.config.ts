38d66daae4e526470700f3d8f11a0651eeb96aa8
Alex J <alexbyj@gmail.com>
2026-08-10 18:20:13 -0500

    Include Sharp binaries in server bundle

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import { withPayload } from "@payloadcms/next/withPayload";
 
 const nextConfig: NextConfig = {
     serverExternalPackages: ['drizzle-kit', '@payloadcms/drizzle', 'sharp'],
+    outputFileTracingIncludes: {
+        '/*': [
+            './node_modules/@img/sharp-linux-x64/**/*',
+            './node_modules/@img/sharp-libvips-linux-x64/**/*',
+            './node_modules/.pnpm/@img+sharp-linux-x64*/node_modules/@img/sharp-linux-x64/**/*',
+            './node_modules/.pnpm/@img+sharp-libvips-linux-x64*/node_modules/@img/sharp-libvips-linux-x64/**/*',
+        ],
+    },
 };
 
 export default withPayload(nextConfig);
