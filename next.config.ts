b6f5647c9e204d6afacd74fb52b4640c5a3c8609
Alex J <alexbyj@gmail.com>
2026-08-10 18:17:46 -0500

    Fix Sharp native runtime deployment

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 import { withPayload } from "@payloadcms/next/withPayload";
 
 const nextConfig: NextConfig = {
-    serverExternalPackages: ['drizzle-kit', '@payloadcms/drizzle'],
+    serverExternalPackages: ['drizzle-kit', '@payloadcms/drizzle', 'sharp'],
 };
 
 export default withPayload(nextConfig);
